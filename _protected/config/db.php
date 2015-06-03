ca39587a22b870149cf68faf781f0754553e7b1a
inCodeSpace <box4goodmail@gmail.com>
2015-06-03 21:21:46 +0400

    add6

--- _protected/config/db.php
+++ _protected/config/db.php
@@ -2,8 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=Yii2_test2',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=mysqlsdb.co8hm2var4k9.eu-west-1.rds.amazonaws.com;dbname=depw5x5m7h8',
+    'username' => 'depw5x5m7h8',
+    'password' => 'nbPDoEF_XNiG',
     'charset' => 'utf8',
 ];
+
+
